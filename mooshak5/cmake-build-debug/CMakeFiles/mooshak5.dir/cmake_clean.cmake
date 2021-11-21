5eb331cd374d9aca1c21e7bcf08e84478fe0524a
Joana <up202006279@edu.fe.up.pt>
2021-11-21 14:26:46 +0000

    added exercise

---
+++ mooshak5/cmake-build-debug/CMakeFiles/mooshak5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mooshak5.dir/main.cpp.obj"
+  "libmooshak5.dll.a"
+  "mooshak5.exe"
+  "mooshak5.exe.manifest"
+  "mooshak5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mooshak5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mooshak5/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+struct Frequencia{
+    char alph =' ';
+    int n=0;
+    Frequencia(char i, int i1) {
+        alph=i; n=i1;
+    }
+};
+
+bool compare(const Frequencia& first, const Frequencia& second){
+    return (first.n>second.n);
+}
+
+int main() {
+
+    list<Frequencia> adn;
+    string in="JSHYNSHQSHDGNSHNSJAHSSA";
+    //cin>>in;
+
+    for(int i=0; i<in.length();i++){
+        list<Frequencia>::iterator it;
+        bool found=false;
+        for ( it=adn.begin(); it != adn.end(); it++){
+            if(it->alph == in[i]) {
+                it->n = it->n + 1;
+                found=true;
+                break;
+            }
+        }
+        if(!found) {
+            Frequencia temp(in[i], 1);
+            adn.push_back(temp);
+        }
+    }
+    adn.sort(compare);
+    for(Frequencia temp:adn){
+        cout<<temp.alph<<" "<<temp.n<<endl;
+    }
+
+    return 0;
+}
